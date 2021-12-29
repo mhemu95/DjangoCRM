a743b857bb0e85fcd253653fb9cb9254d3affcb8
mhemu95 <mohiuddin.hemu@BTSL-Mohiuddin.btraccl.com>
2021-12-29 18:27:11 +0600

    not much changing

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Lead(models.Model):
+    first_name = models.CharField(max_length=20)
+    age = models.IntegerField(default=0)
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
+
+
+class Agent(models.Model):
+    last_name = models.CharField(max_length=20)
