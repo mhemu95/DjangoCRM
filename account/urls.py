c7aad5412a7de2d77a1fea3b180d8ddd3d54e276
mhemu95 <hemu0995@gmail.com>
2022-02-16 18:37:37 +0600

    main.user swapped by account.user remove accoun

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
+from . import views
 
 app_name = 'account'
 
 urlpatterns = [
+    path('register/', views.register, name='register'),
     
 ]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        user = User.objects.create(username=username, email=email, password1=password1, first_name=first_name, last_name=last_name)
+        user.save()
+
+        return redirect('leads:leadList')
+
+    return render(request, 'account/register.html')
